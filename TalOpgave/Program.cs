7557fc8b2ca6a8091dbbfbeb42b0ae8ba2267da2
admin <admin@TA-828-10>
2018-10-29 15:54:17 +0100

    Færdig med dagens opgaver.

---
+++ TalOpgave/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heltal = 10;
+
+            heltal++;
+
+           // Console.WriteLine(heltal);
+
+            heltal--;
+
+           // Console.WriteLine(heltal);
+
+            heltal += 20;
+
+            Console.WriteLine($"Heltal = {heltal}");
+
+            double kommatal = 12.5;
+
+            kommatal++;
+
+            //Console.WriteLine(kommatal);
+
+            kommatal--;
+
+            //Console.WriteLine(kommatal);
+
+            kommatal *= 2;
+
+            Console.WriteLine($"kommatal = {kommatal}");
+
+
+            DebugWait();
+
+        }
+        //This will show Console when using F5 
+        private static void DebugWait()
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ VariableDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int length = 10;
+            double tal = 11111.00;
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(i + " {0}", tal);
+                Console.WriteLine($"Doublemin = {System.Double.MaxValue}");
+                Console.WriteLine($"Doublemax = {System.Double.MinValue}");
+
+            }
+                                
+            DebugWait();
+        }
+
+        //This will show Console when using F5 
+        private static void DebugWait()
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+}
+
+}
+
+
