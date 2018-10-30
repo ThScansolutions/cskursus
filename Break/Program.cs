59fe6fd2f71257f09ed3b5763fd2b6b093ace92b
admin <admin@TA-828-10>
2018-10-30 15:39:34 +0100

    Yderligere opgaver

---
+++ Break/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 3 != 0)
+                {
+
+                    continue;
+                }
+                if (i > 15)
+                { 
+                    break;
+                }
+                Console.WriteLine(i);
+
+            }
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ProgramFlow/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                for (int y = 1; y < 11; y++)
+                                   
+                {
+                    string tal = (i * y).ToString();
+                    if ((i * y) > 50)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        
+                    }
+                    Console.Write(tal.PadLeft(4));
+
+                }
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
