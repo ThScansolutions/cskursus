00599a3a99e0583d2add24218c8091d36d6fd203
admin <admin@TA-828-10>
2018-10-30 15:52:29 +0100

    Oprettet opgave Funktioner

--- Funktioner/Program.cs
+++ Funktioner/Program.cs
@@ -10,7 +10,30 @@ namespace Funktioner
     {
         static void Main(string[] args)
         {
+            int res = Lægsammen(200, 2);
+            Console.WriteLine(res);
 
+            double res1 = Double(2, 2);
+            Console.WriteLine(res1);
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+
+
+        static int Lægsammen(int a, int b)
+        {
+            return a * b;
+        }
+
+
+        static double Double(double a, double b)
+        {
+            return a * b * System.Math.PI;
         }
     }
+   
 }
